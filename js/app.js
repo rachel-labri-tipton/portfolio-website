251cbce1469c295318fa43d9ee5dbfa2a8831bfc
rachel-labri-tipton <rachemtipton@gmail.com>
2023-07-13 21:53:44 +0200

    update background images get slideshow to work for projects

--- js/app.js
+++ js/app.js
@@ -4,9 +4,8 @@ function toggleNavigation() {
     const navUl = ('nav ul');
     navUl.classList.toggle('active')
 }
-
-var slidePosition = 1;
-SlideShow(slidePosition);
+// var slidePosition = 1;
+// SlideShow(slidePosition);
 
 // forward/Back controls
 function plusSlides(n) {
@@ -15,7 +14,12 @@ function plusSlides(n) {
 
 //  images controls
 function currentSlide(n) {
+  
   SlideShow(slidePosition = n);
+}
+
+window.onload = function () {
+  currentSlide(1);
 }
 
 function SlideShow(n) {
@@ -33,3 +37,20 @@ function SlideShow(n) {
   slides[slidePosition-1].style.display = "block";
   circles[slidePosition-1].className += " enable";
 } 
+
+function SlideShowProjects (n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers2");
+  var circles = document.getElementsByClassName("dots2");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  } 
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+}
+
